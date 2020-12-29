3f59198ff6a6f5db3bc947dad69da1b5a7b2afd8
eox-tecnologia <eox-tecnologia>
2020-12-29 11:38:24 -0300

    Alterando lógica de adicionar e criando funcionalidade de deletar

--- index.js
+++ index.js
@@ -1,55 +1,74 @@
+const inputNome = document.querySelector('#name')
+const inputNumero = document.querySelector('#number')
+const buttonAdicionar = document.querySelector('#adicionar')
 const tbody = document.querySelector('tbody')
-const listaPalpites = []
+const inputQtde = document.querySelector('#qtde')
+const buttonSortear = document.querySelector('#sortear')
 
-function createHtml(items) {
-    const tr = document.createElement('tr')
-    const tdNome = document.createElement('td')
-    const tdPalpite = document.createElement('td')
-    const tdAcoes = document.createElement('td')
-    const editIcon = document.createElement('i')
-    const deleteIcon = document.createElement('i')
-    editIcon.classList.add('material-icons')
-    deleteIcon.classList.add('material-icons')
-    editIcon.innerText = 'edit'
-    deleteIcon.innerText = 'delete'
+const listaPalpites = JSON.parse(localStorage.getItem('lista-palpites')) || []
 
-    items.map(obj => {
+function renderizarHtml() {
+    tbody.innerHTML = ''
+    
+    listaPalpites.map(obj => {
+        let tdNome = document.createElement('td')
         tdNome.innerText = obj.nome
+
+        let tdPalpite = document.createElement('td')
         tdPalpite.innerText = obj.palpite
+
+        let editIcon = document.createElement('i')
+        editIcon.classList.add('material-icons')
+        editIcon.innerText = 'edit'
+
+        let deleteIcon = document.createElement('i')
+        deleteIcon.classList.add('material-icons')
+        deleteIcon.innerText = 'delete'
+        let posicao = listaPalpites.indexOf(obj)
+        deleteIcon.setAttribute('onclick', `deletarPalpite(${posicao})`)
+
+        let tdAcoes = document.createElement('td')
         tdAcoes.appendChild(editIcon)
         tdAcoes.appendChild(deleteIcon)
+
+        let tr = document.createElement('tr')
         tr.appendChild(tdNome)
         tr.appendChild(tdPalpite)
         tr.appendChild(tdAcoes)
+
         tbody.appendChild(tr)
+
     })
 }
 
 
-function adicionarPalpite(nome, palpite) {
+function adicionarPalpite() {
+    let nome = inputNome.value
+    let palpite = inputNumero.value
     listaPalpites.push({nome, palpite})
-    createHtml(listaPalpites)
+    inputNome.value = ''
+    inputNumero.value = ''
+    renderizarHtml()
+    saveToStorage()
 }
 
-function editarPalpite() {
-
+function editarPalpite(posicao) {
+    console.log(posicao);
 }
 
-function deletarPalpite() {
+function deletarPalpite(posicao) {
+    listaPalpites.splice(posicao, 1)
+    renderizarHtml()
+    saveToStorage()
+}
 
+function saveToStorage() {
+    localStorage.setItem('lista-palpites', JSON.stringify(listaPalpites))
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit()
-
-    let adicionar = document.querySelector('#adicionar')
-
-    adicionar.addEventListener('click', () => {
-        let nome = document.querySelector('#name').value
-        let palpite = document.querySelector('#number').value
-
-        if(nome && palpite) adicionarPalpite(nome, palpite);
-        else alert('favor preencher nome e palpite')
-    })
+    renderizarHtml()
+    buttonAdicionar.onclick = adicionarPalpite
 });
 
